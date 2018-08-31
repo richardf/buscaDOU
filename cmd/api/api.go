8a444b8a812354cc5cbdc4099a9f7c5de15f2416
y8cf <richard.fuchshuber@petrobras.com.br>
2018-08-31 10:03:39 -0300

    added govalidator

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/asaskevich/govalidator"
 	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
 )
 
 type Response struct {
+	Errors error
+}
+
+type AddKeywordRequest struct {
+	Keyword string `json:"keyword" valid:"length(5|64)"`
+	Email   string `json:"email" valid:"email"`
+}
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
 }
 
 func main() {
@@ -19,7 +31,16 @@ func main() {
 }
 
 func AddKeyword(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "Welcome!")
+	req := AddKeywordRequest{}
+	json.NewDecoder(r.Body).Decode(&req)
+	w.Header().Set("Content-Type", "application/json")
+
+	if ok, err := govalidator.ValidateStruct(req); ok {
+		json.NewEncoder(w).Encode(&Response{Errors: err})
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(&Response{Errors: err})
+	}
 }
 
 func ConfirmEmail(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
